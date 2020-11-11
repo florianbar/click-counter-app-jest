89e9c896567e20a7b2ea92908dc8298d691b16c8
Florian <florianbar01@gmail.com>
2020-11-11 13:24:27 +0200

    Added elements to the App component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 function App() {
   return (
     <div data-test="component-app">
+      <h1 data-test="counter-display">The counter is currently</h1>
+      <button data-test="increment-button">Increment</button>
     </div>
   );
 }
